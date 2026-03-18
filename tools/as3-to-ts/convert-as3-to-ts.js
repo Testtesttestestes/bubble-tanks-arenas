6f78dd2c569b6513adb1e762a53f6336411a255e
Testtesttestestes <ya.cheaterock@yandex.ru>
2026-03-18 15:24:04 +0400

    add Timer stub2

--- tools/as3-to-ts/convert-as3-to-ts.js
+++ tools/as3-to-ts/convert-as3-to-ts.js
@@ -16,7 +16,7 @@ const FLASH_STUB_CLASSES = [
   'NetConnection', 'NetStream', 'LocalConnection', 'Microphone', 'Camera',
   'Matrix3D', 'Vector3D', 'AnimatorFactory3D', 'StageQuality', 
   'URLRequestMethod', 'URLVariables', 'URLLoader',
-  'Mouse', 'MouseCursor',
+  'Mouse', 'MouseCursor', 'Timer',
   'ColorMatrixFilter', 'GlowFilter', 'BlurFilter', 'DropShadowFilter'
 ];
 
